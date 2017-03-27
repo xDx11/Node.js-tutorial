23a40b4d84d85ae307fb7522b1144a4b89d7e6ec
xDx11 <soucek.radek@gmail.com>
2017-03-27 16:18:37 +0200

    Add files via upload

---
+++ routes/index.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+/* GET helloWorld page. */
+router.get('/helloWorld', function(req, res, next) {
+  res.render('helloWorld', { title: 'Hello, World!' });
+});
+
+module.exports = router;
